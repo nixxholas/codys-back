a87d29e711d05404b2584d6601bcd9b056312efb
nixxholas <nixholas@icloud.com>
2024-02-02 12:49:30 +0800

    gooey-back: initial commit
    Signed-off-by: nixxholas <nixholas@icloud.com>

---
+++ gooey-back/src/main/java/com/gooey/gooeyback/GooeyBackApplication.java
@@ -0,0 +1,13 @@
+package com.gooey.gooeyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooeyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GooeyBackApplication.class, args);
+    }
+
+}
---
+++ gooey-back/src/test/java/com/gooey/gooeyback/GooeyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gooey.gooeyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooeyBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
