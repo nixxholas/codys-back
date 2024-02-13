3f14b82841f50959009cc6cb92a790b51e3c3d2c
Codyjtx <jeowcody@gmail.com>
2024-02-13 20:28:24 +0800

    Added additional files
    Moved the player name/number selection to the gameCreationScreen.java.
    
    Main MenuScreen.java now consists of 2 buttons, create public game, that moves to GameCreationScreen, and Exit to Desktop, that quits out of the game.
    
    Added a blackjackologo.png, as i want to add that to the main menu screen.

--- frontend/core/src/com/mygdx/game/screens/GameCreationScreen.java
+++ frontend/core/src/com/mygdx/game/screens/GameCreationScreen.java
@@ -14,16 +14,21 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 
-public class MainMenuScreen extends ScreenAdapter{
+public class GameCreationScreen extends ScreenAdapter{
 
     final Boot game;
     private OrthographicCamera camera;
 
 	private Stage stage;
+    private TextField textField;
 
-    private int proceed = 0;
+    private Label valueLabel, nameLabel;
 
-    public MainMenuScreen(Boot boot){
+    private Slider slider;
+
+    private boolean proceed = false;
+
+    public GameCreationScreen(Boot boot){
         this.game = boot;
         this.camera = boot.camera;
     }
@@ -33,32 +38,55 @@ public class MainMenuScreen extends ScreenAdapter{
         Gdx.input.setInputProcessor(stage);
 
         Skin skin = new Skin(Gdx.files.internal("uiskin.json")); // You can use a different skin
-        
+        // Create a text field
+        TextFieldStyle textFieldStyle = skin.get(TextFieldStyle.class);
+        textField = new TextField("", skin);
+
+
+        // Label
+        nameLabel = new Label("Username", skin);
+        valueLabel = new Label("Number of Players: 1", skin);
+
+        // Create a Slider
+        //Slider.SliderStyle sliderStye = skin.get(Slider.SliderStyle.class);
+        slider = new Slider(1, 6, 1, false, skin);
+
+        // Add a listener to respond to changes in slider value
+        slider.addListener(new ChangeListener() {
+            public void changed(ChangeEvent event, Actor actor) {
+                // Handle slider value change
+                valueLabel.setText(String.format("Number of Players: %d", (int)slider.getValue()));
+            }
+        });
+
         //TextButton
         TextButton btnStart = new TextButton("Start Game", skin);
-        TextButton btnLeave = new TextButton("Exit to Desktop", skin);
         //btnStart.setSize(200, 100);
         btnStart.addListener(new ClickListener() {
             public void touchUp(InputEvent event, float x, float y, int point, int button) {
                 // Handle slider value change
-                proceed = 1;
+                proceed = true;
             }
         });
-        btnLeave.addListener(new ClickListener() {
-            public void touchUp(InputEvent event, float x, float y, int point, int button) {
-                // Handle slider value change
-                proceed = -1;
-            }
-        });
-
 
         // Add text field to a table
         Table table = new Table();
         table.setFillParent(true);
-        table.add(btnStart).width(250).height(50);
+        table.add(nameLabel).top().left();
+        table.row();
         table.add().padTop(20);
         table.row();
-        table.add(btnLeave).width(250).height(50);
+        table.add(textField).width(300).height(40);
+        table.row();
+        table.add().padTop(50);
+        table.row();
+        table.add(valueLabel).top().left();
+        table.row();
+        table.add(slider).width(300).height(50).left();
+        table.row();
+        table.add().padTop(20);
+        table.row();
+        table.add(btnStart).width(250).height(50);
         stage.addActor(table);
 
     }
@@ -75,15 +103,10 @@ public class MainMenuScreen extends ScreenAdapter{
         stage.draw();
         game.batch.end();
 
-        switch (proceed) {
-            case 1:
-                game.setScreen(new GameCreationScreen(game));
-                break;
-            case -1:
-                Gdx.app.exit();
-                System.exit(-1);
-        }
-
+		if (proceed) {
+			game.setScreen(new GameScreen(game));
+			dispose();
+		}
 	}
 
 	@Override
