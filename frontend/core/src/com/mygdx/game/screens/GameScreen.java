b4142c7f41bd3f993ec75561aca85ecb78a570cb
Codyjtx <jeowcody@gmail.com>
2024-02-13 19:14:54 +0800

    More dynamic creation
    Added dynamic creation of cards for rest of the players.
    Reduced repetitive code.
    Started making methods for buttons and labels.

--- frontend/core/src/com/mygdx/game/screens/GameScreen.java
+++ frontend/core/src/com/mygdx/game/screens/GameScreen.java
@@ -41,13 +41,34 @@ public class GameScreen extends ScreenAdapter {
         this.camera = game.camera;
     }
 
-    public void dealCard(float delay, int xPos, int yPos, int offset, int rotation){
+    public void dealVertCards(float delay, int xPos, int yPos, int offset, int rotation){
         Card newC = new Card(backImage);
         newC.setPosition(900, 900);
         stage.addActor(newC);
         SequenceAction sa = newC.cardAnimation(delay, xPos, yPos + offset, rotation, 0.5f, frontImage);
         newC.addAction(sa);
+    }
 
+    public void dealHorizCards(float delay, int xPos, int yPos, int offset, int rotation){
+        Card newC = new Card(backImage);
+        newC.setPosition(900, 900);
+        stage.addActor(newC);
+        SequenceAction sa = newC.cardAnimation(delay, xPos+ offset, yPos , rotation, 0.5f, frontImage);
+        newC.addAction(sa);
+    }
+
+    public void createLabel(){
+        
+    }
+
+    public void createButton(){
+
+    }
+
+    public void createPlayerSet(){
+        dealVertCards(cHeight, cHeight, cHeight, cWidth, cHeight);
+        createLabel();
+        createButton();
     }
 
     @Override
@@ -69,9 +90,9 @@ public class GameScreen extends ScreenAdapter {
 
         // PLAYER 1
 
-        //deal a card, with 0 delay, coord of 1900,1100 offset by cdwidth*2 and rotated 90 degrees
-        for(int i=0; i<=6; i++){
-            dealCard(2.0f * i, 1900, 1100 - (cWidth*2), (cWidth * 1 / 5) * i, 90);
+        //deal 6 cards, with starting 0 delay, coord of 1900,1100 offset by cdwidth*2 and rotated 90 degrees
+        for(int i=0; i<=5; i++){
+            dealVertCards(2.0f * i, 1900, 1000 - (cWidth*2), (cWidth * 1 / 5) * i, 90);
         }
         
         // Card card1 = new Card(backImage);
@@ -118,6 +139,8 @@ public class GameScreen extends ScreenAdapter {
         // PLAYER 1 END
 
         // PLAYER 1 Labels & TextButton
+
+
         Table buttonContainer = new Table(skin);
         buttonContainer.setTransform(true);
         Label lb = new Label("Player 1", skin);
@@ -129,50 +152,14 @@ public class GameScreen extends ScreenAdapter {
         buttonContainer.add(tb).size(100, 50);
         Table rotatingActor = buttonContainer;
         rotatingActor.setRotation(90);
-        rotatingActor.setPosition(1600, 1100 - cWidth);
+        rotatingActor.setPosition(1600, 1000 - cWidth);
         stage.addActor(rotatingActor);
 
         // PLAYER 2
-        Card card1P2 = new Card(backImage);
-        card1P2.setPosition(900, 900);
-        stage.addActor(card1P2);
-        SequenceAction saP2 = card1P2.cardAnimation(12.0f, 1900, 1100 - (cWidth * 5), 90, 0.5f, frontImage);
-        card1P2.addAction(saP2);
-
-        Card card2P2 = new Card(backImage);
-        card2P2.setPosition(900, 900);
-        stage.addActor(card2P2);
-        SequenceAction sa2P2 = card2P2.cardAnimation(14.0f, 1900, 1100 - (cWidth * 5) + (cWidth * 1 / 5), 90, 0.5f,
-                frontImage);
-        card2P2.addAction(sa2P2);
-
-        Card card3P2 = new Card(backImage);
-        card3P2.setPosition(900, 900);
-        stage.addActor(card3P2);
-        SequenceAction sa3P2 = card3P2.cardAnimation(16.0f, 1900, 1100 - (cWidth * 5) + (cWidth * 2 / 5), 90, 0.5f,
-                frontImage);
-        card3P2.addAction(sa3P2);
-
-        Card card4P2 = new Card(backImage);
-        card4P2.setPosition(900, 900);
-        stage.addActor(card4P2);
-        SequenceAction sa4P2 = card4P2.cardAnimation(18.0f, 1900, 1100 - (cWidth * 5) + (cWidth * 3 / 5), 90, 0.5f,
-                frontImage);
-        card4P2.addAction(sa4P2);
-
-        Card card5P2 = new Card(backImage);
-        card5P2.setPosition(900, 900);
-        stage.addActor(card5P2);
-        SequenceAction sa5P2 = card5P2.cardAnimation(20.0f, 1900, 1100 - (cWidth * 5) + (cWidth * 4 / 5), 90, 0.5f,
-                frontImage);
-        card5P2.addAction(sa5P2);
-
-        Card card6P2 = new Card(backImage);
-        card6P2.setPosition(900, 900);
-        stage.addActor(card6P2);
-        SequenceAction sa6P2 = card6P2.cardAnimation(22.0f, 1900, 1100 - (cWidth * 5) + (cWidth * 5 / 5), 90, 0.5f,
-                frontImage);
-        card6P2.addAction(sa6P2);
+        //deal 6 cards, with starting 12.0f delay, coord of 1900,1100 offset by cdwidth*1/5 and rotated 90 degrees
+        for(int i=0; i<=5; i++){
+            dealVertCards(12.0f + 2.0f * i, 1900, 1100 - (cWidth*5), (cWidth * 1 / 5) * i, 90);
+        }
 
         // PLAYER 2 Labels & TextButton
         Table buttonContainer2 = new Table(skin);
@@ -190,41 +177,10 @@ public class GameScreen extends ScreenAdapter {
         stage.addActor(rotatingActor2);
 
         // PLAYER 3
-        Card card1P3 = new Card(backImage);
-        card1P3.setPosition(900, 900);
-        stage.addActor(card1P3);
-        SequenceAction saP3 = card1P3.cardAnimation(24.0f, 1100, 50, 0, 0.5f, frontImage);
-        card1P3.addAction(saP3);
-
-        Card card2P3 = new Card(backImage);
-        card2P3.setPosition(900, 900);
-        stage.addActor(card2P3);
-        SequenceAction sa2P3 = card2P3.cardAnimation(26.0f, 1100 + (cWidth * 1 / 5), 50, 0, 0.5f, frontImage);
-        card2P3.addAction(sa2P3);
-
-        Card card3P3 = new Card(backImage);
-        card3P3.setPosition(900, 900);
-        stage.addActor(card3P3);
-        SequenceAction sa3P3 = card3P3.cardAnimation(28.0f, 1100 + (cWidth * 2 / 5), 50, 0, 0.5f, frontImage);
-        card3P3.addAction(sa3P3);
-
-        Card card4P3 = new Card(backImage);
-        card4P3.setPosition(900, 900);
-        stage.addActor(card4P3);
-        SequenceAction sa4P3 = card4P3.cardAnimation(30.0f, 1100 + (cWidth * 3 / 5), 50, 0, 0.5f, frontImage);
-        card4P3.addAction(sa4P3);
-
-        Card card5P3 = new Card(backImage);
-        card5P3.setPosition(900, 900);
-        stage.addActor(card5P3);
-        SequenceAction sa5P3 = card5P3.cardAnimation(32.0f, 1100 + (cWidth * 4 / 5), 50, 0, 0.5f, frontImage);
-        card5P3.addAction(sa5P3);
-
-        Card card6P3 = new Card(backImage);
-        card6P3.setPosition(900, 900);
-        stage.addActor(card6P3);
-        SequenceAction sa6P3 = card6P3.cardAnimation(34.0f, 1100 + (cWidth * 5 / 5), 50, 0, 0.5f, frontImage);
-        card6P3.addAction(sa6P3);
+        //deal 6 cards, with starting 24.0f delay, coord of 1100,50 offset horizontally by cdwidth*1/5 and rotated 0 degrees
+        for(int i=0; i<=5; i++){
+            dealHorizCards(24.0f + 2.0f * i, 1100, 50, (cWidth * 1 / 5) * i, 0);
+        }
 
         // PLAYER 3 Labels & TextButton
         Table buttonContainer3 = new Table(skin);
@@ -242,41 +198,10 @@ public class GameScreen extends ScreenAdapter {
         stage.addActor(rotatingActor3);
 
         // Player 4
-        Card card1P4 = new Card(backImage);
-        card1P4.setPosition(900, 900);
-        stage.addActor(card1P4);
-        SequenceAction saP4 = card1P4.cardAnimation(36.0f, 500, 50, 0, 0.5f, frontImage);
-        card1P4.addAction(saP4);
-
-        Card card2P4 = new Card(backImage);
-        card2P4.setPosition(900, 900);
-        stage.addActor(card2P4);
-        SequenceAction sa2P4 = card2P4.cardAnimation(38.0f, 500 + (cWidth * 1 / 5), 50, 0, 0.5f, frontImage);
-        card2P4.addAction(sa2P4);
-
-        Card card3P4 = new Card(backImage);
-        card3P4.setPosition(900, 900);
-        stage.addActor(card3P4);
-        SequenceAction sa3P4 = card3P4.cardAnimation(40.0f, 500 + (cWidth * 2 / 5), 50, 0, 0.5f, frontImage);
-        card3P4.addAction(sa3P4);
-
-        Card card4P4 = new Card(backImage);
-        card4P4.setPosition(900, 900);
-        stage.addActor(card4P4);
-        SequenceAction sa4P4 = card4P4.cardAnimation(42.0f, 500 + (cWidth * 3 / 5), 50, 0, 0.5f, frontImage);
-        card4P4.addAction(sa4P4);
-
-        Card card5P4 = new Card(backImage);
-        card5P4.setPosition(900, 900);
-        stage.addActor(card5P4);
-        SequenceAction sa5P4 = card5P4.cardAnimation(44.0f, 500 + (cWidth * 4 / 5), 50, 0, 0.5f, frontImage);
-        card5P4.addAction(sa5P4);
-
-        Card card6P4 = new Card(backImage);
-        card6P4.setPosition(900, 900);
-        stage.addActor(card6P4);
-        SequenceAction sa6P4 = card6P4.cardAnimation(46.0f, 500 + (cWidth * 5 / 5), 50, 0, 0.5f, frontImage);
-        card6P4.addAction(sa6P4);
+        //deal 6 cards, with starting 36.0f delay, coord of 500,50 offset horizontally by cdwidth*1/5 and rotated 0 degrees
+        for(int i=0; i<=5; i++){
+            dealHorizCards(36.0f + 2.0f * i, 500, 50, (cWidth * 1 / 5) * i, 0);
+        }
 
         // PLAYER 4 Labels & TextButton
         Table buttonContainer4 = new Table(skin);
@@ -294,41 +219,10 @@ public class GameScreen extends ScreenAdapter {
         stage.addActor(rotatingActor4);
 
         // Player 5
-        Card card1P5 = new Card(backImage);
-        card1P5.setPosition(900, 900);
-        stage.addActor(card1P5);
-        SequenceAction saP5 = card1P5.cardAnimation(48.0f, 50, 600, -90, 0.5f, frontImage);
-        card1P5.addAction(saP5);
-
-        Card card2P5 = new Card(backImage);
-        card2P5.setPosition(900, 900);
-        stage.addActor(card2P5);
-        SequenceAction sa2P5 = card2P5.cardAnimation(50.0f, 50, 600 - (cWidth * 1 / 5), -90, 0.5f, frontImage);
-        card2P5.addAction(sa2P5);
-
-        Card card3P5 = new Card(backImage);
-        card3P5.setPosition(900, 900);
-        stage.addActor(card3P5);
-        SequenceAction sa3P5 = card3P5.cardAnimation(52.0f, 50, 600 - (cWidth * 2 / 5), -90, 0.5f, frontImage);
-        card3P5.addAction(sa3P5);
-
-        Card card4P5 = new Card(backImage);
-        card4P5.setPosition(900, 900);
-        stage.addActor(card4P5);
-        SequenceAction sa4P5 = card4P5.cardAnimation(54.0f, 50, 600 - (cWidth * 3 / 5), -90, 0.5f, frontImage);
-        card4P5.addAction(sa4P5);
-
-        Card card5P5 = new Card(backImage);
-        card5P5.setPosition(900, 900);
-        stage.addActor(card5P5);
-        SequenceAction sa5P5 = card5P5.cardAnimation(56.0f, 50, 600 - (cWidth * 4 / 5), -90, 0.5f, frontImage);
-        card5P5.addAction(sa5P5);
-
-        Card card6P5 = new Card(backImage);
-        card6P5.setPosition(900, 900);
-        stage.addActor(card6P5);
-        SequenceAction sa6P5 = card6P5.cardAnimation(58.0f, 50, 600 - (cWidth * 5 / 5), -90, 0.5f, frontImage);
-        card6P5.addAction(sa6P5);
+        //deal 6 cards, with starting 48.0f delay, coord of 50,600 offset by -cdwidth*1/5 and rotated -90 degrees
+        for(int i=0; i<=5; i++){
+            dealVertCards(48.0f + 2.0f * i, 50, 600, -(cWidth * 1 / 5) * i, -90);
+        }
 
         // PLAYER 5 Labels & TextButton
         Table buttonContainer5 = new Table(skin);
@@ -346,41 +240,10 @@ public class GameScreen extends ScreenAdapter {
         stage.addActor(rotatingActor5);
 
         // Player 6
-        Card card1P6 = new Card(backImage);
-        card1P6.setPosition(900, 900);
-        stage.addActor(card1P6);
-        SequenceAction saP6 = card1P6.cardAnimation(60.0f, 50, 1100, -90, 0.5f, frontImage);
-        card1P6.addAction(saP6);
-
-        Card card2P6 = new Card(backImage);
-        card2P6.setPosition(900, 900);
-        stage.addActor(card2P6);
-        SequenceAction sa2P6 = card2P6.cardAnimation(62.0f, 50, 1100 - (cWidth * 1 / 5), -90, 0.5f, frontImage);
-        card2P6.addAction(sa2P6);
-
-        Card card3P6 = new Card(backImage);
-        card3P6.setPosition(900, 900);
-        stage.addActor(card3P6);
-        SequenceAction sa3P6 = card3P6.cardAnimation(64.0f, 50, 1100 - (cWidth * 2 / 5), -90, 0.5f, frontImage);
-        card3P6.addAction(sa3P6);
-
-        Card card4P6 = new Card(backImage);
-        card4P6.setPosition(900, 900);
-        stage.addActor(card4P6);
-        SequenceAction sa4P6 = card4P6.cardAnimation(66.0f, 50, 1100 - (cWidth * 3 / 5), -90, 0.5f, frontImage);
-        card4P6.addAction(sa4P6);
-
-        Card card5P6 = new Card(backImage);
-        card5P6.setPosition(900, 900);
-        stage.addActor(card5P6);
-        SequenceAction sa5P6 = card5P6.cardAnimation(68.0f, 50, 1100 - (cWidth * 4 / 5), -90, 0.5f, frontImage);
-        card5P6.addAction(sa5P6);
-
-        Card card6P6 = new Card(backImage);
-        card6P6.setPosition(900, 900);
-        stage.addActor(card6P6);
-        SequenceAction sa6P6 = card6P6.cardAnimation(70.0f, 50, 1100 - (cWidth * 5 / 5), -90, 0.5f, frontImage);
-        card6P6.addAction(sa6P6);
+        //deal 6 cards, with starting 48.0f delay, coord of 50,600 offset by -cdwidth*1/5 and rotated -90 degrees
+        for(int i=0; i<=5; i++){
+            dealVertCards(60.0f + 2.0f * i, 50, 1000, -(cWidth * 1 / 5) * i, -90);
+        }
 
         // PLAYER 6 Labels & TextButton
         Table buttonContainer6 = new Table(skin);
@@ -394,7 +257,7 @@ public class GameScreen extends ScreenAdapter {
         buttonContainer6.add(tb6).size(100, 50);
         Table rotatingActor6 = buttonContainer6;
         rotatingActor6.setRotation(-90);
-        rotatingActor6.setPosition(330, 1100 - cWidth);
+        rotatingActor6.setPosition(330, 1000 - cWidth);
         stage.addActor(rotatingActor6);
     }
 
