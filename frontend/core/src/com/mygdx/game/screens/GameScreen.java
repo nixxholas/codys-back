872ba6f9dae7704e81f48ed4b257a88669deab15
Codyjtx <jeowcody@gmail.com>
2024-02-24 21:59:23 +0800

    Dealing cards based on player's hands
    added array of an string arrays, each containing a player's hand

--- frontend/core/src/com/mygdx/game/screens/GameScreen.java
+++ frontend/core/src/com/mygdx/game/screens/GameScreen.java
@@ -18,6 +18,9 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.Boot;
 import com.mygdx.game.objects.Card;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameScreen extends ScreenAdapter {
     final Boot game;
     private Texture backImage;
@@ -73,7 +76,7 @@ public class GameScreen extends ScreenAdapter {
     @Override
     public void show() {
         backImage = new Texture("back_card_150.png");
-        frontImage = new Texture("2_of_clubs.png");
+        frontImage = new Texture("2_Clubs.png");
 
         cWidth = frontImage.getWidth() + 5;
         cHeight = frontImage.getHeight() + 5;
@@ -110,6 +113,7 @@ public class GameScreen extends ScreenAdapter {
         for (int i = 0; i < numPlayers; i++) {
             // Calculate the angle and position of the sprite
             float angle = MathUtils.lerpAngleDeg(startAngle, startAngle + sweepAngle, i / (float) (numPlayers - 1));
+            // Use cosine and sine to calculate diagonal offset from center of circle
             float x = centerX + MathUtils.cosDeg(angle) * radius;
             float y = centerY + MathUtils.sinDeg(angle) * radius;
             createButtonLabel(skin, (int) x + cWidth + 10, (int) y + cHeight + 40, 0, i+1);
@@ -117,6 +121,30 @@ public class GameScreen extends ScreenAdapter {
                 dealHorizCards(2.0f * z + 12.0f * i, (int)x, (int)y, (cWidth / 5) * z, 0);
             }
         }
+
+        // Situation, server passes client a JSON file.
+        // Client parses it into a list
+        // Add code for that here
+        // *here*
+        // given the following list file of the cards in a player's hand,
+        // deal out cards such that the png used is the actual card value and suit
+        List<String> hand = new ArrayList<String>();
+        hand.add("TWO_DIAMONDS");
+        hand.add("THREE_SPADES");
+        hand.add("FOUR_HEARTS");
+        hand.add("KING_CLUBS");
+        hand.add("ACE_DIAMONDS");
+        List<String> hand2 = new ArrayList<String>();
+        hand.add("TWO_DIAMONDS");
+        hand.add("THREE_SPADES");
+        hand.add("FOUR_HEARTS");
+        hand.add("KING_CLUBS");
+        hand.add("ACE_DIAMONDS");
+        List<List<String>> playerHands = new ArrayList<>(5);
+        playerHands.add(hand);
+        playerHands.add(hand2);
+
+
     }
 
     @Override
