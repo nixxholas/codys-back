94e09c99ea758323cc0c26c3df7e8ceea580c4ef
Sim Kah Hong <skhpotato@proton.me>
2024-02-27 13:00:26 +0800

    Clean up Hud.java

--- frontend/core/src/com/mygdx/game/screens/Hud.java
+++ frontend/core/src/com/mygdx/game/screens/Hud.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
@@ -12,17 +13,21 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 /*
-*   The Hud class is responsible for displaying:
-*   1. Player name
-*   2. Available balance
-*   3. you tell me what else ...
+*   The Hud class is responsible:
+*   1. Display Player name
+*   2. Display and update player balance
+*   3. Display action buttons e.g. Hit, Stand, Double
+*   4. Disable action buttons as necessary
+*
+*   you tell me what else ...
 *
 * */
 public class Hud {
-    public Stage stage;
+    public Stage stage;     /* Stage is used to contain and organize the layout of playerNameTable and balanceTable below. */
+
+    // HUD uses ScreenViewport to maintain its display size regardless of window size
     private final ScreenViewport hudViewport;
     private int balance;
-    private static String playerName;
 
     /*
     *   Set locale to Singapore and language to English.
@@ -33,25 +38,21 @@ public class Hud {
     Locale locale = new Locale.Builder().setLanguage("en").setRegion("SG").build();
     NumberFormat sgCurrencyFormat = NumberFormat.getCurrencyInstance(locale);
 
-    /*
-    *   Labels to label the player name and balance.
-    * */
+    // Labels to hold LITERAL texts
     private static final Label.LabelStyle labelStyle = new Label.LabelStyle(new BitmapFont(), Color.WHITE);
-    static Label playerNameTextLabel = new Label("Player Name: ", labelStyle);
-    static Label balanceTextLabel = new Label("Balance: ", labelStyle);
+    private static final Label playerNameTextLabel = new Label("Player Name: ", labelStyle);
+    private static final Label balanceTextLabel = new Label("Balance: ", labelStyle);
 
-    /*
-    *
-    * */
-    private Table playerNameTable;
-    private Table balanceTable;
+    // Tables to align the labels
+    private final Table playerNameTable;
+    private final Table balanceTable;
 
+    // Labels to hold the values for display
     Label balanceLabel;
     Label playerNameLabel;
 
-    public Hud (SpriteBatch batch, int balance, int bet, String playerName) {
+    public Hud (SpriteBatch batch, int balance, String playerName, Skin skin) {
         this.balance = balance;
-        this.playerName = playerName;
         playerNameLabel = new Label(playerName, labelStyle);
         balanceLabel = new Label(sgCurrencyFormat.format(balance), labelStyle);
 
@@ -59,19 +60,21 @@ public class Hud {
         stage = new Stage(hudViewport, batch);
 
         playerNameTable = new Table();
+        playerNameTable.setSkin(skin);
         playerNameTable.top().left().pad(5); // anchor the table to the top left of the stage with 5px padding above
         playerNameTable.setFillParent(true); // set hudtable to fill the size of the stage
 
         playerNameTable.add(playerNameTextLabel).padRight(10);
-        playerNameTable.add(playerNameLabel);
+        playerNameTable.add(playerName);
 
 
         balanceTable = new Table();
+        balanceTable.setSkin(skin);
         balanceTable.top().right().pad(5); // anchor the table to the top right of the stage with 5px padding above
         balanceTable.setFillParent(true); // set hudtable to fill the size of the stage
 
         balanceTable.add(balanceTextLabel).padRight(10);
-        balanceTable.add(balanceLabel);
+        balanceTable.add(sgCurrencyFormat.format(balance));
 
         stage.addActor(playerNameTable);
         stage.addActor(balanceTable);
