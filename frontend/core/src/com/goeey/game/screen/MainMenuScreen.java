7defddceee061cdcb8b1e0729df73a52e2bc0078
Sim Kah Hong <skhpotato@proton.me>
2024-03-30 18:45:49 +0800

    Added connection check when user clicks Start Game

--- frontend/core/src/com/goeey/game/screen/MainMenuScreen.java
+++ frontend/core/src/com/goeey/game/screen/MainMenuScreen.java
@@ -5,12 +5,11 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.goeey.game.GameManager;
+import com.goeey.game.entity.GameState;
 
 public class MainMenuScreen extends ScreenAdapter {
     private final GameManager game;
@@ -20,18 +19,36 @@ public class MainMenuScreen extends ScreenAdapter {
         this.game = game;
     }
 
-    public Table uiTableFactory() {
-        // Logo
-        Image logoImage = new Image(new Texture("images/blackjacklogo.png"));
-
-        // TextButton
+    public TextButton createStartButton() {
         TextButton startButton = new TextButton("Start Game", game.getSkin());
-        TextButton exitButton = new TextButton("Exit to Desktop", game.getSkin());
         startButton.addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new GameCreationScreen(game));
+                showConnectingDialog();
+
+                // Start the server connection in a separate thread
+                Thread connectToServerThread = new Thread(() -> {
+                    // Perform server connection here
+                    GameManager.socketHandler.establishConnection();
+
+                    // Once the connection is established, dismiss the dialog
+                    Gdx.app.postRunnable(() -> {
+                        stage.getRoot().findActor("connectingDialog").remove();
+                        if(GameState.getGameState().isConnected()) {
+                            game.setScreen(new GameCreationScreen(game));
+                        } else {
+                            showConnectionFailedDialog();
+                        }
+                    });
+                });
+                connectToServerThread.start();
             }
         });
+
+        return startButton;
+    }
+
+    public TextButton createExitButton() {
+        TextButton exitButton = new TextButton("Exit to Desktop", game.getSkin());
         exitButton.addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
                 Gdx.app.exit();
@@ -39,16 +56,53 @@ public class MainMenuScreen extends ScreenAdapter {
             }
         });
 
-        // Prepare Table
+        return exitButton;
+    }
+
+    public Table populateUITable() {
+        // Create Logo Texture
+        Image logoImage = new Image(new Texture("images/blackjacklogo.png"));
+
+        // Add elements to table
         Table uiTable = new Table();
         uiTable.setFillParent(true);
         uiTable.add(logoImage);
         uiTable.row();
-        uiTable.add(startButton).width(500).height(100).padTop(50);
+        uiTable.add(createStartButton()).width(500).height(100).padTop(50);
         uiTable.row();
-        uiTable.add(exitButton).width(500).height(100).padTop(50);
+        uiTable.add(createExitButton()).width(500).height(100).padTop(50);
 
         return uiTable;
+    }
+
+    public void showConnectingDialog() {
+        Dialog connectionDialog = new Dialog("Connect", game.getSkin());
+        connectionDialog.text("Connecting to server ...").center();
+        connectionDialog.setName("connectingDialog");
+
+        connectionDialog.show(stage);
+    }
+
+    private TextButton createOkAndCloseButton(String dialogName) {
+        TextButton okAndCloseButton = new TextButton("Ok", game.getSkin());
+        okAndCloseButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                stage.getRoot().findActor(dialogName).remove();
+            }
+        });
+
+        return okAndCloseButton;
+    }
+
+    public void showConnectionFailedDialog() {
+        Dialog failedConnectionDialog = new Dialog("Error", game.getSkin());
+
+        failedConnectionDialog.text("Failed to connect to server").center();
+        failedConnectionDialog.setName("failedDialog");
+        failedConnectionDialog.button(createOkAndCloseButton(failedConnectionDialog.getName()));
+
+        failedConnectionDialog.show(stage);
     }
 
     public void show() {
@@ -56,7 +110,7 @@ public class MainMenuScreen extends ScreenAdapter {
         stage.setViewport(game.gameViewPort);
         Gdx.input.setInputProcessor(stage);
 
-        stage.addActor(uiTableFactory());
+        stage.addActor(populateUITable());
     }
 
     @Override
@@ -70,4 +124,8 @@ public class MainMenuScreen extends ScreenAdapter {
         stage.getViewport().update(width, height, true);
     }
 
+    @Override
+    public void hide() {
+        dispose();
+    }
 }
