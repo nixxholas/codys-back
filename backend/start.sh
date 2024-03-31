8ea634daf15667c25cb5c4713b9740b98b9eaa76
Codyjtx <jeowcody@gmail.com>
2024-04-01 00:14:13 +0800

    late changes

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 gradle --version
 gradle build --continuous -PskipDownload=true --console=plain &
 gradle bootRun -PskipDownload=true --console=plain
---
+++ frontend/run.bat
@@ -0,0 +1,3 @@
+@REM java -jar Blackjackv1.1.jar "ws://gooey-backend.fly.dev/ws"
+
+./gradlew run  --args="ws://gooey-backend.fly.dev/ws"
